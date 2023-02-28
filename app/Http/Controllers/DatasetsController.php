7f8102fe4eacb1db1689e237657d65a282cccf08
Toby Rea <toby.e.rea@protonmail.com>
2023-02-28 15:00:51 +0000

    Added initial work on download to server functionality

--- app/Http/Controllers/DatasetsController.php
+++ app/Http/Controllers/DatasetsController.php
@@ -14,4 +14,25 @@ class DatasetsController extends Controller
       'datasets' => Dataset::all(),
     ]);
   }
+
+  public function download(string $id)
+  {
+    $dataset = Dataset::where('id', $id)->firstOrFail();
+
+    // Infer the output path from the URL
+    $split_url = explode("/", $dataset->data_url);
+    $file_name = end($split_url);
+    $path = storage_path('app/datasets/') . $file_name;
+
+    // Ensure file isn't already downloaded
+    if (file_exists($path)) {
+      return $file_name . " already downloaded";
+    }
+
+    // Download the file from the URL and store
+    $data = file_get_contents($dataset->data_url);
+    file_put_contents($path, $data);
+
+    return $file_name . " downloaded successfully";
+  }
 }
