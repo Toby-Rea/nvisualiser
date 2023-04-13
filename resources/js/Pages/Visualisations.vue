da7a4bfc21acadc53132bb73091fadd593b74d15
Toby Rea <toby.e.rea@protonmail.com>
2023-04-13 21:45:39 +0100

    Space out the code

--- resources/js/Pages/Visualisations.vue
+++ resources/js/Pages/Visualisations.vue
@@ -7,6 +7,7 @@
   <AuthenticatedLayout>
     <Head title="Visualisations"/>
     <h1 class="text-4xl font-bold tracking-tighter mb-8 px-12 2xl:p-0">Visualisations</h1>
+
     <Link href="/visualisations/create" class="p-2 bg-neutral-100 text-sm hover:scale-105 hover:bg-neutral-200 rounded-md transition-all">
       <p class="font-semibold">Create a new visualisation</p>
     </Link>
