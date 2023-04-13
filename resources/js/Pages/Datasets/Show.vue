ba79e695998ca0d507204735b4b9d6865ca812e3
Toby Rea <toby.e.rea@protonmail.com>
2023-04-13 21:45:27 +0100

    Got parallel coordinates prototype working

--- resources/js/Pages/Datasets/Show.vue
+++ resources/js/Pages/Datasets/Show.vue
@@ -1,11 +1,33 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {Head} from '@inertiajs/vue3';
+import "https://cdn.plot.ly/plotly-2.20.0.js"
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const props = defineProps({
-  data: Object,
   headers: Object,
+  data: Object,
 });
+
+function plot() {
+  // remove the buttton with id plot-btn
+  document.getElementById("plot-btn").remove();
+
+  const chartData = {
+    type: 'parcoords',
+    dimensions: props.headers.map(header => {
+      return {
+        label: header,
+        values: props.data.map(row => row[header])
+      }
+    }),
+    line: {
+      color: 'blue'
+    }
+  };
+
+  Plotly.newPlot("chart", [chartData]);
+}
 </script>
 
 <template>
@@ -13,22 +35,24 @@ const props = defineProps({
   <AuthenticatedLayout>
     <h1 class="text-4xl font-bold tracking-tighter mb-8 px-12 2xl:p-0">Dataset</h1>
 
-    <table>
-      <thead>
-      <tr>
-        <th v-for="header in props.headers">
-          {{ header }}
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="row in props.data">
-        <td v-for="cell in row">
-          {{ cell }}
-        </td>
-      </tr>
-      </tbody>
-    </table>
+    <div id="chart"/>
+    <PrimaryButton id="plot-btn" @click="plot()">Plot</PrimaryButton>
+    <!--    <table>-->
+    <!--      <thead>-->
+    <!--      <tr>-->
+    <!--        <th v-for="header in props.headers">-->
+    <!--          {{ header }}-->
+    <!--        </th>-->
+    <!--      </tr>-->
+    <!--      </thead>-->
+    <!--      <tbody>-->
+    <!--      <tr v-for="row in props.data">-->
+    <!--        <td v-for="cell in row">-->
+    <!--          {{ cell }}-->
+    <!--        </td>-->
+    <!--      </tr>-->
+    <!--      </tbody>-->
+    <!--    </table>-->
 
   </AuthenticatedLayout>
 </template>
