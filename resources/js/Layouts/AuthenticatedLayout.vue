073c3ccc3b616b90ce3d6fdc6df41a1f7dcf9eef
Toby Rea <toby.e.rea@protonmail.com>
2023-04-13 21:44:56 +0100

    Stylistic changes

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,12 +1,13 @@
+<script setup>
+  import Sidebar from "@/Components/Sidebar.vue";
+</script>
+
 <template>
   <div class="w-screen h-full flex">
     <Sidebar/>
 
-    <div class="flex flex-col mx-auto my-8">
+    <div class="flex flex-col m-16 w-full">
       <slot/>
     </div>
   </div>
 </template>
-<script setup>
-import Sidebar from "@/Components/Sidebar.vue";
-</script>
